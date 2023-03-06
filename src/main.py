aa9516f8cfdc1660b2b0206eae7c1c379d1ce3ff
SamuelFLM <felipelimaaa23@gmail.com>
2023-03-06 15:13:08 -0300

    Source

--- src/main.py
+++ src/main.py
@@ -4,6 +4,6 @@ import requests
 # req = requests.get("https://cep-static.awesomeapi.com.br/json/30230522")
 # dic = req.json()
 
-req = requests.get("http://economia.awesomeapi.com.br/json/last/BRL-USD")
+req = requests.get("http://economia.awesomeapi.com.br/json/last/CAD-CLP")
 dic = req.json()
 print(dic)
